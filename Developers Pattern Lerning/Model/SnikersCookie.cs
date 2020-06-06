042fcd77e03bc87a527b101078c64bffb6e99075
Vinniko <vinnik_21@bk.ru>
2020-06-06 14:25:45 +0300

    Add new Cookie and new Factory Commit
    Будет добавлена новая модель SnikersCookie
    Будет добавлена новая фабрика SnikersCookieFactory
    Создание новой фабрики и изготовление новой модели будут добавлены в main

---
+++ Developers Pattern Lerning/Model/SnikersCookie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Developers_Pattern_Lerning.Model
+{
+    class SnikersCookie : Cookie
+    {
+        #region Constructor
+
+        public SnikersCookie(string title) : base(title) { }
+
+        #endregion
+    }
+}
