b8745d6cbb5b0684f46a73471a41035541a14464
Vinniko <vinnik_21@bk.ru>
2021-07-07 10:42:58 +0300

    Create Button model commit

---
+++ Command Method/Models/Button.cs
@@ -0,0 +1,25 @@
+﻿using Command_Method.Interfaces;
+
+namespace Command_Method.Models
+{
+    public sealed class Button : IModel, IInvoker
+    {
+        public void SetCommand(ICommand command)
+        {
+            _command = command;
+        }
+
+        public void Run()
+        {
+            if(_command != null)
+                _command.Execute();
+        }
+
+        public void Cancel()
+        {
+            _command.Undo();
+        }
+
+        private ICommand _command;
+    }
+}
