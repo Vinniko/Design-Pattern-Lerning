dc12903b3d25435b193b24e0aa35ab66c2df70a8
Vinniko <vinnik_21@bk.ru>
2021-07-07 10:44:48 +0300

    Create ConsoleWriter commit

---
+++ Command Method/Services/ConsoleWriter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Command_Method.Interfaces;
+
+namespace Command_Method.Services
+{
+    public sealed class ConsoleWriter : IWriter, IReceiver
+    {
+        public void Write(string text)
+        {
+            Console.WriteLine(text);
+        }
+        public void Operation()
+        {
+            if (!_status)
+            {
+                _status = true;
+                Write("Система включена");
+            }
+            else
+            {
+                _status = false;
+                Write("Система выключена");
+            }
+        }
+
+        private bool _status = false;
+    }
+}
