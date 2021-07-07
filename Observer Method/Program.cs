bc784b74ee2de0c6d329dca60d0d59f4797b3209
Vinniko <vinnik_21@bk.ru>
2021-07-07 09:12:45 +0300

    Realization commit
    Демонстрация работы паттерна Observer

---
+++ Observer Method/Program.cs
@@ -0,0 +1,21 @@
+﻿using Observer_Method.Interfaces;
+using Observer_Method.Services;
+
+namespace Observer_Method
+{
+    sealed class Program
+    {
+        static void Main(string[] args)
+        {
+            IGenerator generator = new MessageGenerator(1, 15);
+            IWriter consoleWriter = new ConsoleWriter();
+            IWriter databaseWriter = new DatabaseWriter();
+            IWriter loger = new Loger();
+            (generator as IObservable).AddObserver(consoleWriter as IObserver);
+            (generator as IObservable).AddObserver(databaseWriter as IObserver);
+            (generator as IObservable).AddObserver(loger as IObserver);
+            for (var i = 0; i < 3; i++)
+                generator.Generate();
+        }
+    }
+}
