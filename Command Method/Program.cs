dbd03a56729475d4c4cbbcec2a2b9709d09b577d
Vinniko <vinnik_21@bk.ru>
2021-07-07 10:45:29 +0300

    Realization of command method commit
    Демонстрация работы паттерна команда

---
+++ Command Method/Program.cs
@@ -0,0 +1,20 @@
+﻿using Command_Method.Interfaces;
+using Command_Method.Models;
+using Command_Method.Services;
+using Command_Method.Commands;
+
+namespace Command_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IReceiver consoleWriter = new ConsoleWriter();
+            ICommand buttonOnClick = new ButtonOnClickCommand(consoleWriter);
+            IInvoker button = new Button();
+            button.SetCommand(buttonOnClick);
+            button.Run();
+            button.Cancel();
+        }
+    }
+}
