64f5b543f7512214e538a89c20b5b47a16c82c6a
Vinniko <vinnik_21@bk.ru>
2021-07-07 09:11:26 +0300

    Create model commit
    Будет создана модель Message

---
+++ Observer Method/Models/Message.cs
@@ -0,0 +1,14 @@
+﻿using Observer_Method.Interfaces;
+
+namespace Observer_Method.Models
+{
+    public sealed class Message : IModel
+    {
+        public override string ToString()
+        {
+            return $"Система сгенерировала сообщение № {Id}: {Text}";
+        }
+        public int Id { get; set; }
+        public string Text {get; set;}
+    }
+}
