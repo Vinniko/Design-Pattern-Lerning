e72f93e2771a19f689a26172d4db3062e1bc6d88
Vinniko <vinnik_21@bk.ru>
2021-07-08 09:13:13 +0300

    Create readers and parsers commit
    Будут созданы классы: FileReader, FileParser, JsonReader, JsonParser

---
+++ Template Method/Services/TxtFileParser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Template_Method.Interfaces;
+
+namespace Template_Method.Services
+{
+    public sealed class TxtFileParser : IParser
+    {
+        public void Pars(string text)
+        {
+            Console.WriteLine("Pars some text from .txt file");
+        }
+    }
+}
---
+++ Template Method/Services/TxtFileReader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Template_Method.Interfaces;
+
+namespace Template_Method.Services
+{
+    public class TxtFileReader : IReader
+    {
+        public string Read(string path)
+        {
+            Console.WriteLine("Reading some .txt file.");
+            return "text";
+        }
+    }
+}
