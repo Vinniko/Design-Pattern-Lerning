1a73bf8b6138385f54004aca5c8cc964bfca66d8
Vinniko <vinnik_21@bk.ru>
2021-07-08 10:25:00 +0300

    Demonstrating Iterator pattern working commit

---
+++ Iterator Method/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Iterator_Method.Interfaces;
+using Iterator_Method.Models;
+using Iterator_Method.Services;
+
+namespace Iterator_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable company = new Company("IT");
+            IEnumerator companyEnumerator = new CompanyEnumerator(company);
+            while (!companyEnumerator.IsDone())
+            {
+                Console.WriteLine($"{(companyEnumerator.Next() as Worker).Name} работает в компании: {(company as Company).Title}");
+            }
+        }
+    }
+}
