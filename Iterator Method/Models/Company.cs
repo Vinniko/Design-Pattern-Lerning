99381b9efdb964204f070338b9f3d3c0e11282b0
Vinniko <vinnik_21@bk.ru>
2021-07-08 10:24:18 +0300

    Create models commit
    Будут созданы модели:
    Worker implements IModel
    Company implemets IModel, IEnumerable

---
+++ Iterator Method/Models/Company.cs
@@ -0,0 +1,36 @@
+﻿using Iterator_Method.Interfaces;
+using Iterator_Method.Services;
+
+namespace Iterator_Method.Models
+{
+    public sealed class Company : IModel, IEnumerable
+    {
+        public Company(string title)
+        {
+            Title = title;
+            _workers = new Worker[] { new Worker("Ben"), new Worker("Jina"), new Worker("Len") };
+        }
+
+        public IEnumerator CreateEnumerator()
+        {
+            return new CompanyEnumerator(this);
+        }
+        public int Count 
+        { 
+            get 
+            {
+                return _workers.Length;
+            } 
+        }
+        public IModel this[int index]
+        {
+            get
+            {
+                return _workers[index];
+            }
+        }
+
+        public string Title { get; set; }
+        private Worker[] _workers;
+    }
+}
