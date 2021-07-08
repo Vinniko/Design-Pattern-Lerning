2825c269d064447965333c8bcfe12ab8c313ee44
Vinniko <vinnik_21@bk.ru>
2021-07-08 09:12:36 +0300

    Create JsonService and FileService wchich implement BaseService commit

---
+++ Template Method/Services/FileService.cs
@@ -0,0 +1,25 @@
+﻿using Template_Method.Interfaces;
+
+namespace Template_Method.Services
+{
+    public sealed class FileService : BaseService
+    {
+        public FileService(string path, IReader reader, IParser parser) : base(path)
+        {
+            _reader = reader;
+            _parser = parser;
+        }
+        public override string Read(string path)
+        {
+            return _reader.Read(path);
+        }
+
+        public override void Pars(string text)
+        {
+            _parser.Pars(text);   
+        }
+
+        private IReader _reader;
+        private IParser _parser;
+    }
+}
