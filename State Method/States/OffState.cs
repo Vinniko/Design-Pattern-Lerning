dc399ca01bc59960671932f712f8f369bf9e9284
Vinniko <vinnik_21@bk.ru>
2021-07-22 09:17:13 +0300

    Create computer states OnState and OffState commit

---
+++ State Method/States/OffState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using State_Method.Interfaces;
+using State_Method.Models;
+
+namespace State_Method.States
+{
+    public sealed class OffState : IState
+    {
+        public void Handle(IModel model)
+        {
+            Console.WriteLine("Computer is turned on");
+            (model as Computer).State = new OnState();
+        }
+    }
+}
---
+++ State Method/States/OnState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using State_Method.Interfaces;
+using State_Method.Models;
+
+namespace State_Method.States
+{
+    public sealed class OnState : IState
+    {
+        public void Handle(IModel model)
+        {
+            Console.WriteLine("Computer is turned off");
+            (model as Computer).State = new OffState();
+        }
+    }
+}
