db95fe1a9b28d9844602c317105b8e7f2eb3fbb5
Vinniko <vinnik_21@bk.ru>
2021-07-29 09:25:12 +0300

    Create AbstractHandler commit

---
+++ Chain of responsibility Method/Handlers/AbstractHandler.cs
@@ -0,0 +1,12 @@
+﻿using Chain_of_responsibility_Method.Interfaces;
+using Chain_of_responsibility_Method.Models;
+
+namespace Chain_of_responsibility_Method.Handlers
+{
+    public abstract class AbstractHandler : IHandler
+    {
+        public abstract void HandleRequest(Task task);
+        public IHandler Successor { get; set; }
+
+    }
+}
