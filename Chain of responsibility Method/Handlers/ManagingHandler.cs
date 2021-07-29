92dc8695e2fe50cca2cf34e431f3e29fee7d9d20
Vinniko <vinnik_21@bk.ru>
2021-07-29 09:25:22 +0300

    Create handlers commit

---
+++ Chain of responsibility Method/Handlers/ManagingHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Chain_of_responsibility_Method.Models;
+
+namespace Chain_of_responsibility_Method.Handlers
+{
+    public class ManagingHandler : AbstractHandler
+    {
+        #region Main Logic
+
+        public override void HandleRequest(Task task)
+        {
+            if (task.TaskName == HANDLER_TASK_NAME)
+            {
+                Console.WriteLine("Manager working with this task");
+            }
+            else if(Successor != null)
+            {
+                Console.WriteLine("Manager set this tusk another worker");
+                Successor.HandleRequest(task);
+            }
+        }
+
+        #endregion
+
+
+
+        #region Fields
+
+        public const string HANDLER_TASK_NAME = "Managing";
+
+        #endregion
+    }
+}
