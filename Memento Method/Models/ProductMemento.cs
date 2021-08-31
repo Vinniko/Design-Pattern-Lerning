8d9e85a5e712b472d2a6e8ced9be5ce8449465cc
Vinniko <vinnik_21@bk.ru>
2021-08-31 12:33:26 +0300

    Create ProductMemento class commit

---
+++ Memento Method/Models/ProductMemento.cs
@@ -0,0 +1,11 @@
+﻿using Memento_Method.Interfaces;
+
+namespace Memento_Method.Models
+{
+    public class ProductMemento : IModel
+    {
+        public string Title { get; set; }
+        public int Qty { get; set; }
+        public decimal Price { get; set; }
+    }
+}
