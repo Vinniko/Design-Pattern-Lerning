476755fff05496905908ac09eef1e1cf9d2f5ac9
Vinniko <vinnik_21@bk.ru>
2021-08-31 09:29:14 +0300

    Create context model commit

---
+++ Interpreter Method/Models/Context.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using Interpreter_Method.Interfaces;
+
+namespace Interpreter_Method.Models
+{
+    public class Context : IContext
+    {
+        #region Contructors
+
+        public Context(Dictionary<string, string> args)
+        {
+            _params_names = args;
+        }
+
+        #endregion
+
+
+        
+        #region Main Logic
+
+        public string getValueByName(string name)
+        {
+            if (_params_names.ContainsKey(name))
+            {
+                return _params_names[name];
+            }
+
+            return null;
+        }
+
+        public void setValue(string name, string value)
+        {
+            if (_params_names.ContainsKey(name))
+            {
+                _params_names[name] = value;
+            }
+            else
+            {
+                _params_names.Add(name, value);
+            }
+        }
+
+        #endregion
+        
+        
+        
+        #region Fields
+
+        private readonly Dictionary<string, string> _params_names;
+
+        #endregion
+    }
+}
