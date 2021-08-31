982e1748b5aeded38b55e970b5d58e46090056f9
Vinniko <vinnik_21@bk.ru>
2021-08-31 10:28:33 +0300

    Demonstrating Mediator pattern work commit

---
+++ Mediator Method/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Mediator_Method.Models;
+
+namespace Mediator_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mediator mediator = new Mediator();
+            AbstractMediableModel guitar = new Guitar(mediator);
+            AbstractMediableModel musician = new Musician(mediator);
+            mediator.Musician = musician;
+            mediator.Guitar = guitar;
+            
+            musician.Send("ДО");
+            guitar.Send("ДО");
+            musician.Send("РЕ");
+            guitar.Send("МИ");
+        }
+    }
+}
