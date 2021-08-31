063164f62b1636398e924fd8be1631826b1bd928
Vinniko <vinnik_21@bk.ru>
2021-08-31 12:33:43 +0300

    Create ProductHistory for product memento classes commit

---
+++ Memento Method/Models/ProductHistory.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Memento_Method.Interfaces;
+
+namespace Memento_Method.Models
+{
+    public class ProductHistory
+    {
+        #region Constructors
+
+        public ProductHistory()
+        {
+            MementoProducts = new Stack<IModel>();
+        }
+
+        #endregion
+
+
+
+        #region Fields
+
+        public Stack<IModel> MementoProducts { get; private set; }
+
+        #endregion
+        
+    }
+}
