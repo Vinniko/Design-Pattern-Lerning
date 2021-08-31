09adb8b02f6df3fe524945416629f814da78e175
Vinniko <vinnik_21@bk.ru>
2021-08-31 12:32:53 +0300

    Create AbstractMementableModel commit

---
+++ Memento Method/Models/AbstractMementableModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Memento_Method.Interfaces;
+
+namespace Memento_Method.Models
+{
+    public abstract class AbstractMementableModel : IModel
+    {
+        #region Main Logic
+
+        public abstract IModel SaveState();
+
+        public abstract void RestoreState(IModel model);
+
+        #endregion
+
+
+
+        #region Fields
+
+        protected IModel _memento;
+
+        #endregion
+    }
+}
