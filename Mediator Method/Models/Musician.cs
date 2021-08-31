7bff36335b8c6605248d9d36b14c0225d6e7d94f
Vinniko <vinnik_21@bk.ru>
2021-08-31 10:27:57 +0300

    Create models and mediator commit

---
+++ Mediator Method/Models/Musician.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Mediator_Method.Interfaces;
+
+namespace Mediator_Method.Models
+{
+    public class Musician : AbstractMediableModel
+    {
+        #region Constructors
+
+        public Musician(IMediator mediator)
+            : base(mediator)
+        { }
+
+        #endregion
+
+
+
+        #region Main Logic
+
+        public override void Realize(string message)
+        {
+            Console.WriteLine($"Музыкан слышит ноту: {message}");
+        }
+
+        #endregion
+    }
+}
