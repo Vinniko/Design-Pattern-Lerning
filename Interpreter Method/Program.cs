0b8ee138f13e36b1bffb6b6c1072956e9279aeed
Vinniko <vinnik_21@bk.ru>
2021-08-31 09:29:51 +0300

    Demostrating Interpreter method work commit

---
+++ Interpreter Method/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Interpreter_Method.Models;
+using Interpreter_Method.Services;
+
+namespace Interpreter_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово:");
+            string value = Console.ReadLine();
+            string name = "word";
+            var context = new Context(new Dictionary<string, string>()
+            {
+                {name, value},
+            });
+            var wordExpression = new WordExpression(name);
+            var firstLetterToUpperExpression = new FirstLetterToUpperExpression(wordExpression);
+            firstLetterToUpperExpression.Interpret(context);
+            
+            Console.WriteLine(context.getValueByName(name));
+        }
+    }
+}
