c9063757b350c54150494c0765da896af287c13d
Vinniko <vinnik_21@bk.ru>
2021-08-31 09:29:28 +0300

    Create expressions commit

---
+++ Interpreter Method/Services/FirstLetterToUpperExpression.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using Interpreter_Method.Interfaces;
+using Interpreter_Method.Models;
+
+namespace Interpreter_Method.Services
+{
+    public class FirstLetterToUpperExpression : IExpression
+    {
+        #region Constructors
+
+        public FirstLetterToUpperExpression(IExpression wordExpression)
+        {
+            _wordExpression = wordExpression;
+        }
+
+        #endregion
+
+
+
+        #region Main Logic
+
+        public void Interpret(IContext context)
+        {
+            string value = (context as Context).getValueByName((_wordExpression as WordExpression).getName());
+            (context as Context).setValue((_wordExpression as WordExpression).getName(),
+                value.First().ToString().ToUpper() + value.Substring(1));
+        }
+
+        #endregion
+
+
+
+        #region Fields
+
+        private IExpression _wordExpression;
+
+        #endregion
+    }
+}
---
+++ Interpreter Method/Services/WordExpression.cs
@@ -0,0 +1,46 @@
+﻿using Interpreter_Method.Interfaces;
+
+namespace Interpreter_Method.Services
+{
+    public class WordExpression : IExpression
+    {
+        #region Constructors
+
+        public WordExpression(string name)
+        {
+            _name = name;
+        }
+
+        #endregion
+
+
+
+        #region Main Logic
+
+        public void Interpret(IContext context)
+        {
+            
+        }
+
+        #endregion
+
+
+
+        #region Get/Set
+
+        public string getName()
+        {
+            return _name;
+        }
+
+        #endregion
+
+
+
+        #region Fields
+
+        private string _name;
+
+        #endregion
+    }
+}
